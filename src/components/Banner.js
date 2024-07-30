8f0bce1c3a2a99dacab309fc4c4e315fa0efaba9
unknown <iwanhprasetyo14@gmail.com>
2024-07-30 17:04:54 +0700

    'setup'

---
+++ src/components/Banner.js
@@ -0,0 +1,73 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImage from "../assets/img/header-img.svg";
+
+export const Banner = () => {
+  const [loopNumber, setLoopNumber] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer "];
+  const [text, setText] = useState("");
+  const periode = 2000;
+  const [delta, setDelta] = useState(300 - Math.random() * 300);
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => clearInterval(ticker);
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNumber % toRotate.length;
+    let fullText = toRotate[i];
+    let updateText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+    setText(updateText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+    if (!isDeleting && updateText === fullText) {
+      setIsDeleting(true);
+      setDelta(periode);
+    } else if (isDeleting && updateText === "") {
+      setIsDeleting(false);
+      setLoopNumber(loopNumber + 1);
+      setDelta(500);
+    }
+  };
+
+  return (
+    <section className="Banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">Welcome to my Portofolio</span>
+            <h1>
+              {"Hi I'm  Iwan H. Prasetyo "}
+              <span className="wrap">{text}</span>
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <button onClick={() => console.log("jalan klik")}>
+              Let's Connect <ArrowRightCircle size={25} />
+            </button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImage} alt="header Img" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
