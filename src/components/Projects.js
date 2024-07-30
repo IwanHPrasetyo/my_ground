eb1cd8691f91b1934b60ef79b206fbcef68106ba
unknown <iwanhprasetyo14@gmail.com>
2024-07-30 19:07:01 +0700

    'project_fix'

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,9 @@
 import { Container, Row, Col, Nav, Tab } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import { ColorSharp2 } from "../assets/img/color-sharp2.png";
-import { projImg1 } from "../assets/img/project-img1.png";
-import { projImg2 } from "../assets/img/project-img2.png";
-import { projImg3 } from "../assets/img/project-img3.png";
+import ColorSharp2 from "../assets/img/color-sharp2.png";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
 
 export const Projects = () => {
   const projects = [
@@ -50,7 +50,11 @@ export const Projects = () => {
               eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </p>
             <Tab.Container id="projects-tabs" defaultActiveKey="first">
-              <Nav variant="pills" defaultActiveKey="/home">
+              <Nav
+                variant="pills"
+                className="nav-pills mb-5 justify-content-center align-items-center"
+                id="pills-tab"
+              >
                 <Nav.Item>
                   <Nav.Link eventKey="first">Tab One</Nav.Link>
                 </Nav.Item>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,12 +4,11 @@ import "react-multi-carousel/lib/styles.css";
 import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
-import colorSharp from "../assets/img/color-sharp.png";
+import colorSharp from "../assets/img/color-sharp2.png";
 
 export const Skills = () => {
   const responsive = {
     superLargeDesktop: {
-      // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
       items: 5,
     },
@@ -70,7 +69,7 @@ export const Skills = () => {
           </Col>
         </Row>
       </Container>
-      <img className="backgorund-image-left" src={colorSharp} />
+      <img className="background-image-left" src={colorSharp} />
     </section>
   );
 };
